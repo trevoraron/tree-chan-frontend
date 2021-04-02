3bb8b381a1b01ae2077a66e84d2dea498e215caa
trevoraron <trevoraron13@gmail.com>
2021-04-02 15:41:52 -0700

    buttons and owners

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import {Container, Card} from 'react-bootstrap'
 
 const contractAddress = "0xFc2540389af95921a8c2D8AeA3A884bf9e38cee8";
 const contractMethods = [
-  "function getMessage(uint256 _token) external view returns (string message)"
+  "function getMessage(uint256 _token) external view returns (string message)",
+  "function ownerOf(uint256 _tokenId) external view returns (address)",
+  "function getParents(uint256 _token) external view returns (uint256[] memory)",
+  "function getBranches(uint256 _token) external view returns (uint256[] memory)",
+  "function newThread(string memory _message) public returns (uint256)",
+  "function comment(uint256 _post, string memory _message)",
 ];
 
 declare global {
@@ -43,7 +48,7 @@ function App() {
     </Button>
   } else {
     element = <p>User Address: {address}</p>
-    post = <Post id={1} signer={signer}></Post>
+    post = <PostManager id={4} signer={signer}></PostManager>
   }
 
   return (
@@ -59,12 +64,13 @@ function App() {
   );
 }
 
-type PostProps = {
+type PostManagerProps = {
   id: Number 
   signer: Signer
 }
-function Post(props: PostProps) {
-  const [message, setMessage] = useState("")
+function PostManager(props: PostManagerProps) {
+  const [id, updateMainID] = useState(props.id)
+  const [parents, updateParents] = useState<Number[]>([])
 
   useEffect(() =>  {
     let contract = new ethers.Contract(
@@ -73,15 +79,51 @@ function Post(props: PostProps) {
       props.signer
     )
     const fetchData = async () => {
+      let curParents = await contract.getParents(id)
+      console.log(curParents)
+      updateParents(curParents)
+    }
+    fetchData()
+  }, [id, props.signer])
+
+  return <Post id={id} signer={props.signer} updateID={updateMainID}></Post>
+}
+
+type PostProps = {
+  id: Number 
+  signer: Signer
+  updateID: React.Dispatch<React.SetStateAction<Number>>
+}
+
+function Post(props: PostProps) {
+  const [message, setMessage] = useState("")
+  const [owner, setOwner] = useState("")
+
+  useEffect(() =>  {
+    let contract = new ethers.Contract(
+      contractAddress,
+      contractMethods,
+      props.signer
+    )
+    const fetchData = async () => {
+      let readOwner = await contract.ownerOf(props.id)
       let readMessage = await contract.getMessage(props.id)
       setMessage(readMessage)
+      setOwner(readOwner)
     }
     fetchData()
   }, [props.id, props.signer])
 
   let messageDisplayed = <Spinner animation="border" role="status"></Spinner>
   if(message !== "") {
-    messageDisplayed = <Card.Text>{message}</Card.Text>
+    messageDisplayed = <>
+      <Card.Subtitle>Owner: {owner}</Card.Subtitle>
+      <Card.Text>{message}</Card.Text>
+    </>
+  }
+
+  function makePrimary() {
+    props.updateID(props.id)
   }
 
   return (
@@ -90,6 +132,7 @@ function Post(props: PostProps) {
         <Card.Title>Post #:{props.id}</Card.Title>
         {messageDisplayed}
         <Button variant="primary">Comment</Button>
+        <Button variant="secondary" onClick={() => makePrimary()}>Make Primary</Button>
       </Card.Body>
     </Card>
   )
