a3ddd7ade68df7ec4571383463ad4f8a629539a8
trevoraron <trevoraron13@gmail.com>
2021-04-02 12:51:54 -0700

    getting the base working

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React, { useState } from 'react';
 // import './App.css';
-import { Jumbotron, Button, Spinner } from 'react-bootstrap'
+import { Jumbotron, Button, Spinner } from 'react-bootstrap';
+import { ethers, Signer } from 'ethers';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 function App() {
   const [waiting, setWaiting] = useState(false)
+  const [signer, setSigner] = useState<Signer | null>(null)
+  const [address, setAddress] = useState("")
 
-  let element = <Spinner animation="border" role="status"></Spinner>
-  if (!waiting) {
-    element = <Button onClick={async () => {setWaiting(true)}}>
-        Connect Wallet
-      </Button>
+  async function enableMetamask() {
+    setWaiting(true)
+    await window.ethereum.enable()
+    let provider = new ethers.providers.Web3Provider(window.ethereum)
+    let currSigner = provider.getSigner()
+    let address = await currSigner.getAddress()
+    console.log("Got Address!")
+    console.log(address)
+    setAddress(address)
+    setSigner(currSigner)
+    setWaiting(false)
+  }
+
+  let element = <></>
+  if (waiting) {
+    element = <Spinner animation="border" role="status"></Spinner>
+  } else if (signer == null) {
+    element = <Button onClick={async () => {await enableMetamask()}}>
+      Connect Wallet
+    </Button>
+  } else {
+    element = <p>User Address: {address}</p>
   }
 
   return (
