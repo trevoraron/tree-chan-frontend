bd5cda29d5acb80dde4a13859d0955aaaacf9a60
trevoraron <trevoraron13@gmail.com>
2021-04-02 19:17:13 -0700

    new thread ability

--- src/PostManager.tsx
+++ src/PostManager.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { BigNumber, ethers, Signer } from 'ethers';
 import { Col, Row, Form} from 'react-bootstrap'
 import { ContractAddress, ContractMethods } from './Constants';
+import { Card, Spinner, Button } from 'react-bootstrap'
 import { Post } from './Post';
 
 
@@ -45,6 +46,8 @@ function PostManager(props: PostManagerProps) {
     }
 
     return <>
+        <NewThread signer={props.signer} updateID={updateMainID}></NewThread>
+        &nbsp;
         <Form onSubmit={handleSubmit}>
             <Form.Group as={Row}>
             <Col sm={2}>
@@ -62,6 +65,60 @@ function PostManager(props: PostManagerProps) {
         <h3>Replies</h3>
         {replies.map(p => <Post id={p} signer={props.signer} updateID={updateMainID}></Post>)}
     </>
+}
+
+type NewThreadProps = {
+    signer: Signer
+    updateID: React.Dispatch<React.SetStateAction<BigNumber>>
+}
+function NewThread(props: NewThreadProps) {
+    const [sendingTx, setSendingTx] = useState(false)
+    const [comment, setComment] = useState("")
+
+    function handleSubmit(event: any) {
+        event.preventDefault();
+    }
+
+    async function newThread() {
+        let contract = new ethers.Contract(
+            ContractAddress,
+            ContractMethods,
+            props.signer
+        )
+        const tx = await contract.newThread(comment)
+        console.log(`Transaction hash: ${tx.hash}`);
+        setSendingTx(true)
+    
+        const receipt = await tx.wait();
+        console.log(`Transaction confirmed in block ${receipt.blockNumber}`);
+        let iface = new ethers.utils.Interface(ContractMethods);
+        let parsed = iface.parseLog(receipt.events[0]);
+        let tokenID = parsed.args._tokenId
+        console.log(`New Token ID: ${tokenID}`)
+        setComment("")
+        setSendingTx(false)
+        props.updateID(tokenID)
+    }
+
+    let commentBox = (
+        <Form onSubmit={handleSubmit}>
+            <Form.Control as="textarea" rows={2} value={comment} onChange={(e) => setComment(e.target.value)}/>
+        </Form>
+    )
+    if(sendingTx) {
+        commentBox = <Spinner animation="border" role="status"></Spinner>
+    }
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>New Thread</Card.Title>
+                {commentBox}
+                <Button variant="primary" onClick={async () => {await newThread()}}>NewThread</Button>
+            </Card.Body>
+        </Card>
+    )
+
 }
 
 export {PostManager};
